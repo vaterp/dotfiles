8e4c183f44bc7e35f34ff43534848f76038c4d76
Brian Snyder <bsnyder@idirect.net>
2013-01-04 18:57:33 -0500

    Update for full set of links

--- .linky.sh
+++ .linky.sh
@@ -1,10 +1,20 @@
 ln -s dotfiles/.profile
 ln -s dotfiles/.inputrc
 ln -s dotfiles/.gitconfig
-ln -s dotfiles/.vim/
 ln -s dotfiles/.zshrc
 ln -s dotfiles/.vimrc
 ln -s dotfiles/.screenrc
 ln -s dotfiles/.alias
 ln -s dotfiles/.bashrc
 ln -s dotfiles/.gitignore
+ln -s dotfiles/.screenpioneer
+ln -s dotfiles/.screenresearch
+ln -s dotfiles/.tmux.conf 
+ln -s dotfiles/.cvsrc
+ln -s dotfiles/.xmodmap
+
+
+ln -s dotfiles/.vim/
+
+
+#Note this doesnt link up .ssh/config or .config/xfce4
