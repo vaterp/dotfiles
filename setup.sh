39d87f06ef0cc20d918bda7c3953d6aad6156572
Brian Snyder <bsnyder@idirect.net>
2017-06-29 15:27:26 -0400

    cleanup for osx

--- setup.sh
+++ setup.sh
@@ -1,11 +1,10 @@
-
-ln -s dotfiles/.inputrc
-ln -s dotfiles/.tmux.conf 
+ln -s dotfiles/.bash_aliases
+ln -s dotfiles/.bash_profile
+ln -s dotfiles/.bashrc
 ln -s dotfiles/.gitconfig
 ln -s dotfiles/.gitignore
-ln -s dotfiles/.bash_aliases
-
-
+ln -s dotfiles/.inputrc
+ln -s dotfiles/.tmux.conf 
 ln -s dotfiles/.vimrc
 ln -s dotfiles/.vim/
 
