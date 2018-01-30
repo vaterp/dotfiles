5251b67f1636af299df9ab83173ec88840b7eaf4
Brian Snyder <brian.snyder@gmail.com>
2018-01-30 05:47:07 -0800

    Setup stuff for new gnome machines

--- setup.sh
+++ setup.sh
@@ -1,24 +1,28 @@
-mv ~/.inputrc ~/.inputrc-original 2>/dev/null
-
+#mv ~/.inputrc ~/.inputrc-original 2>/dev/null
 #mv ~/.profile ~/.profile-original 2>/dev/null
 #ln -s dotfiles/.profile
 #ln -s dotfiles/.xmodmap
 
+#Setup my main files
 ln -s dotfiles/.inputrc
 ln -s dotfiles/.tmux.conf 
 ln -s dotfiles/.gitconfig
 ln -s dotfiles/.gitignore
 ln -s dotfiles/.bash_aliases
-
-
 ln -s dotfiles/.vimrc
 ln -s dotfiles/.vim/
-
-
-#Note this doesnt link up .ssh/config or .config/xfce4
 
 #
 #UBU ONLY, just cut and paste the following:
 #
-#sudo apt install git vim htop vim-gnome tmux 
+
+#sudo apt install git vim htop vim-gnome tmux  mc  dconf-editor
 #sudo apt install fbreader iftop netstat
+
+#GNOME:
+
+#gsettings set org.gnome.desktop.wm.preferences raise-on-click false
+#gsettings set org.gnome.desktop.wm.preferences resize-with-right-button true
+#gsettings set org.gnome.desktop.wm.preferences focus-mode 'mouse'
+
+
