2fd08b1e2447674925ea147593e429f906e5dd7e
Brian Snyder <brian.snyder@gmail.com>
2018-07-09 12:32:43 -0400

    Making setup.sh script runnable by default with other comments for dif scenarios

--- setup.sh
+++ setup.sh
@@ -1,7 +1,4 @@
 #mv ~/.inputrc ~/.inputrc-original 2>/dev/null
-#mv ~/.profile ~/.profile-original 2>/dev/null
-#ln -s dotfiles/.profile
-#ln -s dotfiles/.xmodmap
 
 #Setup my main files
 ln -s dotfiles/.inputrc
@@ -19,7 +16,7 @@ ln -s dotfiles/.vim/
 #sudo apt install git vim gvim-gtk htop tmux  mc  dconf-editor
 #sudo apt install fbreader iftop netstat
 
-#GNOME:
+####GNOME:
 #gsettings set org.gnome.desktop.wm.preferences raise-on-click false
 #gsettings set org.gnome.desktop.wm.preferences resize-with-right-button true
 #gsettings set org.gnome.desktop.wm.preferences focus-mode 'mouse'
@@ -28,7 +25,7 @@ ln -s dotfiles/.vim/
 #dconf load /org/gnome/ < gnome-backup.txt
 
 
-# Notes on machine setup....
+#### Notes on machine setup....
 #git clone https://github.com/vaterp/bin.git
 #git clone -b [freebsd|osx|develop] https://github.com/vaterp/dotfiles.git
 #git clone https://github.com/vaterp/help.git
@@ -37,8 +34,12 @@ ln -s dotfiles/.vim/
 #To setup SUDOER with no PASSWD
 #USERNAME ALL=(ALL) NOPASSWD:ALL
 
+####If not VM but main desktop, want to setup .xmodmap and call it from .profile
+#mv ~/.profile ~/.profile-original 2>/dev/null
+#ln -s dotfiles/.profile
+#ln -s dotfiles/.xmodmap
 
-#Notes on VM that can help
+####Notes on VM that can help
 #(Too startup with window size hints, must run vmware-user)
 #/usr/lib/vmware-tools/sbin64/vmtoolsd -n vmusr --blockFd 3
 #sudo /etc/init.d/vmware-tools status
